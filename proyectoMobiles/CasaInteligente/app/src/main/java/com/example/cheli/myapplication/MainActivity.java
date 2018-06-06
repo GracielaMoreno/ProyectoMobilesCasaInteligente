e91c849020b87cb9ace59977a1ebb391716883d5
CaroCald <carolina.2594@hotmail.com>
2018-06-05 21:33:15 -0500

    logeo gmail

--- proyectoMobiles/CasaInteligente/app/src/main/java/com/example/cheli/myapplication/MainActivity.java
+++ proyectoMobiles/CasaInteligente/app/src/main/java/com/example/cheli/myapplication/MainActivity.java
@@ -1,13 +1,62 @@
 package com.example.cheli.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
 
 public class MainActivity extends AppCompatActivity {
-
+    private GoogleApiClient googleApiClient;
+    private final int CODERC = 9001;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SignInButton botongoogle = (SignInButton) findViewById(R.id.logeogmail);
+        botongoogle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                logeoGmail();
+            }
+        });
     }
+
+    public void logeoGmail() {
+
+        if (googleApiClient != null) {
+            googleApiClient.disconnect();
+        }
+        GoogleSignInOptions googleSignInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
+        googleApiClient = new GoogleApiClient.Builder(this).addApi(Auth.GOOGLE_SIGN_IN_API, googleSignInOptions).build();
+        Intent signIntent = Auth.GoogleSignInApi.getSignInIntent(googleApiClient);
+        startActivityForResult(signIntent, CODERC);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == CODERC) {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            if (result.isSuccess()) {
+                GoogleSignInAccount acc = result.getSignInAccount();
+                Intent intent = new Intent(getApplicationContext(), Menu.class);
+                startActivity(intent);
+
+            } else {
+                Toast.makeText(this, "Error en ingreso con GMAIl", Toast.LENGTH_LONG).show();
+
+
+            }
+        }
+    }
+
 }
