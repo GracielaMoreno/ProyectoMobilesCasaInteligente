187c6cf76f99e0d44e83decee062c93a0ec7dc05
CaroCald <carolina.2594@hotmail.com>
2018-06-05 21:26:13 -0500

    navegacion en menu
    clic en imagenes

--- proyectoMobiles/CasaInteligente/app/src/main/java/com/example/cheli/myapplication/Menu.java
+++ proyectoMobiles/CasaInteligente/app/src/main/java/com/example/cheli/myapplication/Menu.java
@@ -1,7 +1,9 @@
 package com.example.cheli.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Menu extends AppCompatActivity {
 
@@ -11,5 +13,33 @@ public class Menu extends AppCompatActivity {
         setContentView(R.layout.activity_menu);
     }
 
+    public void pantallaSala(View view){
 
+        Intent intent = new Intent(getApplicationContext(), sala.class);
+        startActivity(intent);
+    }
+
+    public void pantallaCocina(View view){
+
+        Intent intent = new Intent(getApplicationContext(), cocina.class);
+        startActivity(intent);
+    }
+
+    public void pantallaHabitacion(View view){
+
+        Intent intent = new Intent(getApplicationContext(), habitaciones.class);
+        startActivity(intent);
+    }
+
+    public void pantallaAlarma(View view){
+        Intent intent = new Intent(getApplicationContext(), alarma.class);
+        startActivity(intent);
+
+    }
+
+    public void pantallaAyuda(View view){
+        Intent intent = new Intent(getApplicationContext(), Ayuda.class);
+        startActivity(intent);
+
+    }
 }
